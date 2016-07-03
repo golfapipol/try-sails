92687c9fc41f72e63bf99cd95bb48d345834e082
Apipol Sukgler <apipol@computerlogy.com>
2016-07-03 22:49:03 +0700

    generate model pirate & ship

---
+++ api/controllers/PirateController.js
@@ -0,0 +1,11 @@
+/**
+ * PirateController
+ *
+ * @description :: Server-side logic for managing pirates
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
